73d38aa96145660a21183767bb569682d70f5a19
sevenwell <tmon.seveny@gmail.com>
2020-03-23 22:18:35 +0900

    CSSを追加

--- load.js
+++ load.js
@@ -1,33 +1,35 @@
 function printCard(activity) {
 
     var content = document.getElementById('cardTemplate').content;
+    content.querySelector('a').href = "https://www.youtube.com/watch?v=" + activity['contentDetails']['upload']['videoId'];
     content.querySelector('img').src = activity['snippet']['thumbnails']['medium']['url'];
     content.querySelector('p').textContent = activity['snippet']['title'];
-    document.getElementById('board').appendChild(document.importNode(content, true));
+    document.getElementById('videos').appendChild(document.importNode(content, true));
 
 }
 
-function requestAPI(id) {
+function requestAPI(channel) {
+
     gapi.client.youtube.activities.list({
-      'part': 'snippet',
-      'channelId': id
+      'part': 'snippet,contentDetails',
+      'channelId': channel['id']
     }).then(function(response) {
 
       var activities = response.result.items;
 
       for (var j = 0; j < activities.length; j++) {
-
         var activity = activities[j];
         if (activity['snippet']['type'] == 'upload') {
             printCard(activity);
+            break;
         }
-    
       }
+
     });
 }
 
 function load() {
-    for (let name in channels) {
-        requestAPI(channels[name]);
+    for (var i = 0; i < channels.length; i++) {
+        requestAPI(channels[i]);
     }
 }
